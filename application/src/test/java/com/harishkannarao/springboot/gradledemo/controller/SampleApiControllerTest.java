8abefe0a1cd189252287d0170fd4bb233f5142d6
harishkannarao <harishkannarao@users.noreply.github.com>
2021-12-18 12:04:00 +0000

    Using spring mockMvc to test web layer

--- application/src/test/java/com/harishkannarao/springboot/gradledemo/controller/SampleApiControllerTest.java
+++ application/src/test/java/com/harishkannarao/springboot/gradledemo/controller/SampleApiControllerTest.java
@@ -1,20 +1,27 @@
 package com.harishkannarao.springboot.gradledemo.controller;
 
-import com.harishkannarao.springboot.gradledemo.dto.SampleResponseDto;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SuppressWarnings("ConstantConditions")
 public class SampleApiControllerTest {
     private final SampleApiController underTest = new SampleApiController();
+    private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
 
     @Test
-    void verify_response() {
-        ResponseEntity<SampleResponseDto> result = underTest.getSampleResponse();
-        assertThat(result.getStatusCodeValue(), equalTo(200));
-        assertThat(result.getBody().message(), equalTo("My Sample Api Message"));
+    void verify_response() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/api/sample"))
+                .andReturn()
+                .getResponse();
+        assertThat(response.getStatus(), equalTo(200));
+        DocumentContext jsonResult = JsonPath.parse(response.getContentAsString());
+        assertThat(jsonResult.read("$.message"), equalTo("My Sample Api Message"));
     }
 }
--- application/src/test/java/com/harishkannarao/springboot/gradledemo/controller/SampleWebControllerTest.java
+++ application/src/test/java/com/harishkannarao/springboot/gradledemo/controller/SampleWebControllerTest.java
@@ -1,18 +1,25 @@
 package com.harishkannarao.springboot.gradledemo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ModelAndView;
 
+import static java.util.Objects.requireNonNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class SampleWebControllerTest {
     private final SampleWebController underTest = new SampleWebController();
+    private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
 
     @Test
-    void verify_response() {
-        ModelAndView result = underTest.getSampleHtml();
-        assertThat(result.getViewName(), equalTo("web_template/sample_page"));
+    void verify_response() throws Exception {
+        ModelAndView result = mockMvc.perform(get("/web/sample"))
+                .andReturn()
+                .getModelAndView();
+        assertThat(requireNonNull(result).getViewName(), equalTo("web_template/sample_page"));
         assertThat(result.getModel().get("message"), equalTo("My Sample Web Message"));
     }
 }
